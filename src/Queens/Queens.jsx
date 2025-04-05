299ffe5c656dccf15b1b8737d6a18d2b971f649d
Tautvydas Peleckas <tapel@danskebank.lt>
2025-04-05 20:55:00 +0300

    feat: add Queens component with input and button for grid generation

---
+++ src/Queens/Queens.jsx
@@ -0,0 +1,14 @@
+import './Queens.css';
+
+function Queens() {
+
+
+    
+
+    return (
+        <>
+        <input type="text" placeholder="Enter number of queens" />
+        <button>Generate grid</button>
+        </>
+    )
+}
